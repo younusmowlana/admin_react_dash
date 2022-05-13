1ba7f27df4187b8bff9d96b5778aa1e5d96b3c55
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-05-13 15:29:15 +0530

    updated

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,31 +1,44 @@
-import React, { useState } from 'react'
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { login } from "../../Redux/apiCalls";
 
 const Login = () => {
-    const [username,setUsername] = useState("");
-    const [password,setPassword] = useState("");
-    
-    const handleClick = (e) => {
-        e.preventDefault();
-        login(dispatch, { username, password });
-      };
-    
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    login(dispatch, { username, password });
+  };
 
   return (
-    <div>
-      <input type="text"
-        placeholder='username'
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <input
+        style={{ padding: 10, marginBottom: 20 }}
+        type="text"
+        placeholder="username"
         onChange={(e) => setUsername(e.target.value)}
-        />
-
-      <input type="password" 
-        placeholder='password' 
+      />
+      <input
+        style={{ padding: 10, marginBottom: 20 }}
+        type="password"
+        placeholder="password"
         onChange={(e) => setPassword(e.target.value)}
-        />
+      />
       <button onClick={handleClick} style={{ padding: 10, width:100 }}>
         Login
       </button>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
