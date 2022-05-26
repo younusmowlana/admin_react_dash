e0e3d4d83ecc903dfbc60c710d69499a7224981e
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-05-26 18:36:11 +0530

    Updated

--- src/pages/userList/UserList.jsx
+++ src/pages/userList/UserList.jsx
@@ -2,18 +2,38 @@ import "./userList.css";
 import { DataGrid } from "@material-ui/data-grid";
 import { DeleteOutline } from "@material-ui/icons";
 import { userRows } from "../../dummyData";
-import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useState , useEffect} from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getUser } from "../../Redux/apiCalls";
 
 export default function UserList() {
+  const dispatch = useDispatch();
   const [data, setData] = useState(userRows);
+  const {user} = useSelector((state) => state.user);
+  console.log(user)
+  // const [users, setUsers] = useState([]);
+
+
+  useEffect(() => {
+    getUser(dispatch);
+  }, [dispatch]);
+
+  // useEffect(() => {
+  //   const getUsers = async () => {
+  //     try {
+  //       const res = await userRequest.get("user/?new=true");
+  //       setUsers(res.data);
+  //     } catch {}
+  //   };
+  //   getUsers();
+  // }, []);
 
   const handleDelete = (id) => {
     setData(data.filter((item) => item.id !== id));
   };
   
   const columns = [
-    { field: "id", headerName: "ID", width: 90 },
+    { field: "_id", headerName: "ID", width: 90 },
     {
       field: "user",
       headerName: "User",
@@ -45,9 +65,9 @@ export default function UserList() {
       renderCell: (params) => {
         return (
           <>
-            <Link to={"/user/" + params.row.id}>
+            {/* <Link to={"/user/" + params.row.id}>
               <button className="userListEdit">Edit</button>
-            </Link>
+            </Link> */}
             <DeleteOutline
               className="userListDelete"
               onClick={() => handleDelete(params.row.id)}
@@ -60,13 +80,16 @@ export default function UserList() {
 
   return (
     <div className="userList">
+      {user ? 
       <DataGrid
-        rows={data}
-        disableSelectionOnClick
-        columns={columns}
-        pageSize={8}
-        checkboxSelection
-      />
+          rows={user}
+          disableSelectionOnClick
+          columns={columns}
+          pageSize={8}
+          getRowId={(row) => row._id}
+          checkboxSelection
+      /> : <h1>No data</h1>
+}
     </div>
   );
 }
