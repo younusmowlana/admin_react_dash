3a3ae939b9f98a999b34551938dbd9fb33592338
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-05-26 18:36:27 +0530

    Updated

--- src/Redux/userRedux.js
+++ src/Redux/userRedux.js
@@ -6,6 +6,7 @@ const userSlice = createSlice({
     currentUser: null,
     isFetching: false,
     error: false,
+    user: [],
   },
   reducers: {
     loginStart: (state) => {
@@ -22,8 +23,41 @@ const userSlice = createSlice({
     logout: (state) => {
       state.currentUser = null;
     },
+    //Get all users
+    getUserStart: (state) => {
+      state.isFetching = true;
+      state.error = false;
+    },
+    getUserSuccess: (state, action) => {
+      state.isFetching = false;
+      state.user = action.payload;
+    },
+    getUserFailure: (state) => {
+      state.isFetching = false;
+      state.error = true;
+    },
+    //DELETE
+    deleteUserStart: (state) => {
+      state.isFetching = true;
+      state.error = false;
+    },
+    deleteUserSuccess: (state, action) => {
+      state.isFetching = false;
+      state.user.splice(
+        state.user.findIndex((item) => item._id === action.payload),
+        1
+      );
+    },
+    deleteUserFailure: (state) => {
+      state.isFetching = false;
+      state.error = true;
+    },
+
+
+
+
   },
 });
 
-export const { loginStart, loginSuccess, loginFailure } = userSlice.actions;
+export const { loginStart, loginSuccess, loginFailure, getUserStart, getUserSuccess, getUserFailure, deleteUserStart, deleteUserSuccess, deleteUserFailure  } = userSlice.actions;
 export default userSlice.reducer;
