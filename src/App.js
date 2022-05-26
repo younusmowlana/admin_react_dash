ce14e1ad76ae65bc092af9b97dffde2be3baccf3
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-05-27 00:30:42 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import { useSelector } from "react-redux";
 
 
 function App() {
-   const admin = useSelector((state) => state.user.currentUser.isAdmin);
+  const admin = useSelector((state) => state.user.currentUser.isAdmin);
   //  const admin = true;
   //const admin = true;
   return (
