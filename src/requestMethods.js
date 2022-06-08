e9d320ef598a741483fce831a1db7833d19b594b
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-06-08 14:17:11 +0530

    updated

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -4,7 +4,7 @@ const BASE_URL = "http://localhost:5000/api/";
 // const TOKEN = 
 //     JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
 //      .accessToken || "";
-const TOKEN =  JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
+const TOKEN =  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNGViN2VjM2M2MmMzMmI5MDljZDliMSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY1NDYyNTU1MiwiZXhwIjoxNjU0ODg0NzUyfQ.hSxz-fgeFCi7WoQ3FJ4qHlcwkQc8hR-k6lliZ7WwHLY";
 // console.log(TOKEN);
 // console.log(
 //     JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken
